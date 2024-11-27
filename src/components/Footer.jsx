dc457d6ac87c75ba4316ee48c092cefef6abd186
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-11-27 19:38:08 +0530

    change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,16 +25,10 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           {/* About Section */}
-          <div className="col-md-4 mb-3">
-            <h5>About Us</h5>
-            <p >
-             We are a leading Digital Marketing Institute in faridabad. Feel free to contact to know more about us!
-            </p>
-          </div>
-
+          
           {/* Quick Links Section */}
           <div className="col-md-4 mb-3">
-            <h5>Quick Links</h5>
+            <h5>Company</h5>
             <ul className="list-unstyled">
               <li><a href="/home"  style={{color:"white",textDecoration:"none"}} >Home</a></li>
               {/* <li><a href="/about" style={{color:"white",textDecoration:"none"}} >About</a></li> */}
@@ -49,7 +43,7 @@ const Footer = () => {
 
           {/* Social Media Section */}
           <div className="col-md-4 mb-3">
-            <h5>Follow Us</h5>
+            <h5>We Are Social</h5>
             <ul className="list-unstyled flex">
               <li><a href="https://facebook.com" style={{color:"white",textDecoration:"none"}}  ><i className="bi bi-facebook"></i> Facebook</a></li>
               <li><a href="https://twitter.com" style={{color:"white",textDecoration:"none"}}><i className="bi bi-twitter"></i> Twitter</a></li>
@@ -59,6 +53,9 @@ const Footer = () => {
         </div>
 
         <hr className="bg-light"/>
+
+
+
 
         {/* Copyright Section */}
         <div className="text-center py-3">
